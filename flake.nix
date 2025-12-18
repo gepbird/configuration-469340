3fcf6abfcf8156926734f89454bc80faba2faa09
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-12-18 23:06:43 +0100

    Remove many packages for faster build

--- flake.nix
+++ flake.nix
@@ -33,36 +33,104 @@
     ];
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      blipa = nixpkgs.lib.nixosSystem {
-        pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-        specialArgs = { inherit inputs; };
-        modules = [
-          { networking.hostName = "blipa"; }
-          inputs.disko.nixosModules.disko
-          inputs.lanzaboote.nixosModules.lanzaboote
-          ./hosts/blipa
-        ];
-      };
-      # `nix run .#nixosConfigurations.blipa-vm.config.system.build.vm`
-      blipa-vm = nixpkgs.lib.nixosSystem {
-        pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-        specialArgs = { inherit inputs; };
-        modules = [
-          { networking.hostName = "blipa-vm"; }
-          inputs.disko.nixosModules.disko
-          inputs.lanzaboote.nixosModules.lanzaboote
-          ./hosts/blipa
-          {
-            boot.lanzaboote.enable = nixpkgs.lib.mkForce false;
-            boot.loader.systemd-boot.enable = nixpkgs.lib.mkForce true;
-            disko.devices = nixpkgs.lib.mkForce {};
-            users.users.arthur.hashedPassword = nixpkgs.lib.mkForce null;
-            users.users.arthur.initialPassword = "arthur";
-          }
-        ];
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations = {
+        blipa = nixpkgs.lib.nixosSystem {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          specialArgs = { inherit inputs; };
+          modules = [
+            { networking.hostName = "blipa"; }
+            inputs.disko.nixosModules.disko
+            inputs.lanzaboote.nixosModules.lanzaboote
+            ./hosts/blipa
+          ];
+        };
+        # `nix run .#nixosConfigurations.blipa-vm.config.system.build.vm`
+        blipa-vm = nixpkgs.lib.nixosSystem {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            config.allowUnfree = true;
+          };
+          specialArgs = { inherit inputs; };
+          modules = [
+            { networking.hostName = "blipa-vm"; }
+            inputs.disko.nixosModules.disko
+            inputs.lanzaboote.nixosModules.lanzaboote
+            ./hosts/blipa
+            (
+              { pkgs, ... }:
+              {
+                boot.lanzaboote.enable = nixpkgs.lib.mkForce false;
+                boot.loader.systemd-boot.enable = nixpkgs.lib.mkForce true;
+                disko.devices = nixpkgs.lib.mkForce { };
+                users.users.arthur.hashedPassword = nixpkgs.lib.mkForce null;
+                users.users.arthur.initialPassword = "arthur";
+                virtualisation.vmVariant.virtualisation.memorySize = 4096;
+                services.getty.autologinUser = "arthur";
+                environment.systemPackages = with pkgs; [
+                  xfce.xfce4-terminal
+                ];
+                services.gnome.core-apps.enable = false;
+                services.gnome.core-developer-tools.enable = false;
+                services.gnome.games.enable = false;
+                services.gnome.gnome-browser-connector.enable = false;
+                environment.gnome.excludePackages = with pkgs; [
+                  gnome-tour
+                  gnome-user-docs
+                  baobab      # disk usage analyzer
+                  cheese      # photo booth
+                  eog         # image viewer
+                  epiphany    # web browser
+                  gedit       # text editor
+                  simple-scan # document scanner
+                  totem       # video player
+                  yelp        # help viewer
+                  evince      # document viewer
+                  file-roller # archive manager
+                  geary       # email client
+                  seahorse    # password manager
+                  gnome-calculator
+                  gnome-maps
+                  gnome-console
+                  gnome-calendar
+                  gnome-contacts
+                ];
+                services.gnome.evolution-data-server.enable = nixpkgs.lib.mkForce false;
+                #services.xserver.displayManager.autoLogin.enable = true;
+                #services.xserver.displayManager.autoLogin.user = "arthur";
+                nixpkgs.overlays = [
+                  (
+                    final: prev:
+                    let
+                      pkgs' = import (prev.fetchzip {
+                        url = "https://github.com/NixOS/nixpkgs/archive/16db70aea3e6af351e8ad4c217745ec67d46c8ca.tar.gz";
+                        hash = "sha256-aUjQebbh+0SaZF0VMWtWF+DJAYx5IMbsor8F+VZQhDk=";
+                      }) { inherit (prev) system; };
+                    in
+                    {
+                      inherit (pkgs')
+                        webkitgtk_4_1
+                        webkitgtk_6_0
+                        #libadwaita
+                        #gjs
+                        gnome-control-center
+                        libsecret
+                        ;
+                      pipewire = prev.pipewire.overrideAttrs { doCheck = false; };
+                      libdrm = prev.libdrm.override { withValgrind = false; };
+                      mesa = prev.mesa.override { withValgrind = false; };
+                    }
+                  )
+                ];
+              }
+            )
+          ];
+        };
       };
     };
-  };
 }
--- hosts/blipa/default.nix
+++ hosts/blipa/default.nix
@@ -2,33 +2,11 @@
 
 {
   imports = [
-    # Import hardware configuration
-    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
-
     ./disko-configuration.nix
     ./hardware-configuration.nix
   ];
 
-  boot.initrd.supportedFilesystems = [ "zfs" ];
-  boot.initrd.kernelModules = [ "zfs" ];
-
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.forceImportAll = false;
-  # Enabled by default, but recommended to disable.
-  # If it won't boot without, setting kernel parameters is required.
-  boot.zfs.forceImportRoot = false;
-
   networking.hostId = "f1e73287";
-
-  # LUKS encryption with automatic unlock
-  boot.initrd.systemd.enable = true;
-
-  # Use Lanzaboote for secure boot
-  boot.loader.systemd-boot.enable = false;
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
-  };
 
   users.users.arthur = {
     isNormalUser = true;
@@ -43,64 +21,18 @@
 
   networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Amsterdam";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
-
   # Ensure clean /tmp on boot
-  boot.tmp.cleanOnBoot = true;
+  #boot.tmp.cleanOnBoot = true;
 
   # Enable flakes and new CLI
   nix.settings = {
       experimental-features = [ "nix-command" "flakes" ];
   };
 
-  # Install git
-  environment.systemPackages = with pkgs; [ git ];
-
-  # Request Chromium and Electron to run with Wayland
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   services.xserver.videoDrivers = [ "modesetting" ];
   hardware.graphics.enable = true;
 
   console.useXkbConfig = true;
-
-  # Enable sound.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable the fwupd daemon for firmware updates
-  services.fwupd.enable = true;
-
-  fonts.enableDefaultPackages = true;
-  fonts.enableGhostscriptFonts = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
